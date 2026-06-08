39970073dd8c3afc425cbde82210fd392e790242
Gaffer Fitch <gaffer.fitch@gmail.com>
2026-06-08 15:54:10 -0400

    Pinned version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     install_requires=[
         'requests==2.25.1',
-        'relations-dil==0.6.14'
+        'relations-dil==0.6.15'
     ],
     url="https://github.com/relations-dil/python-relations-rest",
     author="Gaffer Fitch",
